43210a022cddd31b138a8806bdfc799dbd72ad92
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-07-12 19:15:05 -0300

    exercicio 03 feito

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,16 @@
 const jogadores = ["Guido", "Dani", "Ruli", "Diego", "Vidal"];
+const tempo = (10 / jogadores.length) * 1000;
+let contador = 0;
+
+const rodada = () => {
+    if (contador < jogadores.length) {
+        console.log(jogadores[contador]);
+        contador++;
+    } else {
+        console.log("A rodada terminou!");
+        clearInterval(id);
+    }
+
+};
+
+const id = setInterval((rodada), tempo);
