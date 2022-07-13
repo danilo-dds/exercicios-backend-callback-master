8727fc5b1babb82977bd179610c8356b95397599
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-07-12 21:04:36 -0300

    exercicio 04 feito

--- 04/index.js
+++ 04/index.js
@@ -2,5 +2,17 @@
 
 let tempoParaAlarmarEmSegundos = 10;
 let tempoAlarmandoEmSegundos = 5;
+let contador = 0;
 
+console.log(`Timer iniciado, disparando alarme em ${tempoParaAlarmarEmSegundos} segundos`);
+const inicioAlarme = () => {
+    contador++;
+    console.log(contador);
 
+    if (contador >= tempoParaAlarmarEmSegundos && contador <= (tempoParaAlarmarEmSegundos + tempoAlarmandoEmSegundos)) {
+        console.log("Beep beep!");
+    } else if (contador > tempoParaAlarmarEmSegundos + tempoAlarmandoEmSegundos) {
+        clearInterval(id);
+    }
+};
+const id = setInterval((inicioAlarme), 1000);
